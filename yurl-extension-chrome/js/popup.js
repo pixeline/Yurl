76a8d18b164cf762a2d227b83a6b450ade4dac95
Alexandre Plennevaux <alexandre@pixeline.be>
2015-11-09 23:39:34 +0100

    History + Favicons

--- yurl-extension-chrome/js/popup.js
+++ yurl-extension-chrome/js/popup.js
@@ -23,59 +23,65 @@ var delay = (function() {
 	};
 })();
 /*
+	Function that renders the bookmark node tree
+*/
+renderTheResults = function(results) {
+	total = results.length;
+	var str = '';
+	$('#total')[0].innerHTML = total + ' urls';
+	if (total > 0) {
+		var html = [];
+		for (var i = 0; i < total; i++) {
+			var node = results[i];
+			var className = (node.children) ? 'folder' : 'link';
+			if (className !== 'folder' && (typeof node.url !== 'undefined')) {
+				// handle bookmarklets correctly
+				node.descr = (node.url.substring(0, 10) === "javascript") ? 'Bookmarklet' : node.url;
+				node.favicon = (node.descr === 'Bookmarklet')?  '': '<img class="favicon" src="chrome://favicon/'+node.url+'"> ';
+				str = '<li tabindex="' + (i + 1) + '" class="bookmark"><a href="' + node.url + '"><h3>'+node.favicon + node.title + '</h3></a><p>' + node.descr.trunc(55, true) + '<span data-bookmark="' + node.id + '" class="delete" title="Delete Bookmark"></span></p></li>';
+				html.push(str);
+			}
+		}
+		myHTMLString = '<ul id="bookmarkslist">' + html.join('') + '</ul>';
+	} else {
+		myHTMLString = '<p class="error">Sorry, no result.</p>';
+	}
+	//THIS IS FASTER THAN JQUERY'S HTML() function
+	$('#bookmarks')[0].innerHTML = myHTMLString;
+	yurl_cache = JSON.stringify({
+		search: query,
+		results: myHTMLString,
+		timestamp: new Date().getTime()
+	});
+	localStorage.setItem('yurl', yurl_cache);
+}
+/*
 
-	RUNTIME	
-	
+	RUNTIME
+
 */
 // Search the bookmarks when entering the search keyword.
 jQuery(function($) {
 	var query = '',
 		myHTMLString = '',
 		total = 0;
-		
-	if (localStorage.getItem("yurl") !== null ) {
-		
+	if (localStorage.getItem("yurl") !== null) {
 		var yurl_cache = JSON.parse(localStorage.getItem("yurl"));
 		var now = new Date().getTime().toString();
-		
-		if ( yurl_cache && yurl_cache.search && yurl_cache.results && yurl_cache.timestamp && ((now - yurl_cache.timestamp.toString()) < 120000) ) {
+		if (yurl_cache && yurl_cache.search && yurl_cache.results && yurl_cache.timestamp && ((now - yurl_cache.timestamp.toString()) < 120000)) {
 			$('#search').val(yurl_cache.search);
 			$('#bookmarks')[0].innerHTML = yurl_cache.results;
 		}
 	}
+	$('#history').on('click', function() {
+		$('#bookmarks').empty();
+		chrome.bookmarks.getRecent(10, renderTheResults);
+	});
 	$('#search').keyup(function() {
 		delay(function() {
 			$('#bookmarks').empty();
 			query = $('#search').val();
-			chrome.bookmarks.search(query, function(results) {
-				total = results.length;
-				var str = '';
-				$('#total')[0].innerHTML = total + ' urls';
-				if (total > 0) {
-					var html = [];
-					for (var i = 0; i < total; i++) {
-						var node = results[i];
-						var className = (node.children) ? 'folder' : 'link';
-						if (className !== 'folder' && (typeof node.url !== 'undefined')) {
-							// handle bookmarklets correctly
-							node.descr = (node.url.substring(0, 10) === "javascript") ? 'Bookmarklet' : node.url;
-							str = '<li tabindex="' + (i + 1) + '" class="bookmark"><a href="' + node.url + '"><h3>' + node.title + '</h3></a><p>' + node.descr.trunc(55, true) + '<span data-bookmark="' + node.id + '" class="delete" title="Delete Bookmark"></span></p></li>';
-							html.push(str);
-						}
-					}
-					myHTMLString = '<ul id="bookmarkslist">' + html.join('') + '</ul>';
-				} else {
-					myHTMLString = '<p class="error">Sorry, no result.</p>';
-				}
-				//THIS IS FASTER THAN JQUERY'S HTML() function
-				$('#bookmarks')[0].innerHTML = myHTMLString;
-				yurl_cache = JSON.stringify({
-					search: query,
-					results: myHTMLString,
-					timestamp: new Date().getTime()
-				});
-				localStorage.setItem('yurl', yurl_cache);
-			});
+			chrome.bookmarks.search(query, renderTheResults);
 		}, 400);
 		// End keyup callback
 	});
@@ -107,7 +113,7 @@ jQuery(function($) {
 			$(this).prev('.bookmark').focus();
 		}
 		if (e.which === 40) {
-			// Arrow Down 
+			// Arrow Down
 			$(this).next('.bookmark').focus();
 		}
 		if (e.which === 9) {
